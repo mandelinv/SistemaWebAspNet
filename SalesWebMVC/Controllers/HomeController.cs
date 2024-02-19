a774892f33bf993efe41daf2fdf436cb1d63b4e2
Gustavo Mandel <gustavo.mandel@inventti.com.br>
2024-02-19 14:29:09 -0300

    First controller and Razor pager tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Student"] = "Gustavo Mandel";
 
             return View();
         }
