681d8cda868af185c13a75bb59980077d0a172ea
Gustavo Mandel <gustavo.mandel@inventti.com.br>
2024-02-29 16:09:35 -0300

    Update seller and custom service exception

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,16 +1,15 @@
 ﻿using SalesWebMVC.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
     public class SellerService
     {
         private readonly SalesWebMVCContext _context;
-        
+
         public SellerService(SalesWebMVCContext context)
         {
             _context = context;
@@ -37,6 +36,23 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e) // Exceção a nível de acesso a dados
+            {
+                throw new DbConcurrencyException(e.Message); // Exceção a nível de serviço
+            }
         }
     }
 }
